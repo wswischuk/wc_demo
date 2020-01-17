6a0f95b2ed1d4922ebbe3e56c0612d0f2f8c4c8f
Nicolas Swischuk <nws10@duke.edu>
2020-01-17 12:56:57 -0500

    updated calculator code

--- calculator.py
+++ calculator.py
@@ -2,20 +2,26 @@
 
 def add(x, y):
     z = x + y
-    print("{} + {} = {}".format(x, y, z))
-    return z
+    symbol = "+"
+    return z, symbol
+
 def sub(x, y):
     z = x - y
     print("{} - {} = {}".format(x, y, z))
     return z
 def mult(x, y):
     z = x * y
+    if z > 100:
+        return z
     print("{} * {} = {}".format(x, y, z))
 
 x = input("Enter a letter: ")
 print("You entered {}".format(x))
 if x == "a":
-    d = add(56, 73)
+    d, f = add(56, 73)
+    print("{} {} {} = {}".format(57, f, 73, d))
+    if d is None:
+       print("I forgot a variable in add()")
 elif x == "s":
    e = sub(56, 73)
 elif x == "m":
